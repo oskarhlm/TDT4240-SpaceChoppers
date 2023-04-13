265ecdd6f8f07788af00ccce841b7756024eb119
Karl Oskar Magnus Holm <koholm@stud.ntnu.no>
2023-04-13 14:32:16 +0200

    Add main menu screen

--- SpaceChoppers/core/src/com/mygdx/spacechoppers/SpaceChoppersGame.kt
+++ SpaceChoppers/core/src/com/mygdx/spacechoppers/SpaceChoppersGame.kt
@@ -22,6 +22,7 @@ class SpaceChoppersGame : ApplicationAdapter() {
         sb = SpriteBatch()
         gsm = GameStateManager(this)
         gsm.push(MainMenuState(gsm))
+//        gsm.push(TestMenuState(gsm))
     }
 
     override fun render() {
