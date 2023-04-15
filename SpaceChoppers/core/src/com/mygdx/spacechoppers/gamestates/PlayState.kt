9f5fddc8df88b9775e3d027c1f555519ae800efe
Arild Grimstveit <arild@grimstveit.no>
2023-04-15 03:53:06 +0200

    Render Box2D world using the debug renderer

--- SpaceChoppers/core/src/com/mygdx/spacechoppers/gamestates/PlayState.kt
+++ SpaceChoppers/core/src/com/mygdx/spacechoppers/gamestates/PlayState.kt
@@ -1,11 +1,9 @@
 package com.mygdx.spacechoppers.gamestates
 
-import com.badlogic.gdx.Screen
 import com.badlogic.gdx.Gdx
 import com.badlogic.gdx.graphics.GL20
 import com.badlogic.gdx.graphics.Texture
-import com.badlogic.gdx.graphics.g2d.TextureRegion
-import com.badlogic.gdx.graphics.glutils.ShapeRenderer
+import com.badlogic.gdx.math.Matrix4
 import com.badlogic.gdx.math.Vector2
 import com.badlogic.gdx.math.Vector3
 import com.badlogic.gdx.physics.box2d.Box2DDebugRenderer
@@ -16,13 +14,12 @@ import com.mygdx.spacechoppers.GameContactListener
 import com.mygdx.spacechoppers.GameState
 import com.mygdx.spacechoppers.GameStateManager
 import com.mygdx.spacechoppers.controller.ChopperController
+import com.mygdx.spacechoppers.helper.Const.PIXELS_TO_METERS
 import com.mygdx.spacechoppers.model.AsteroidModel
-import com.mygdx.spacechoppers.model.AsteroidTextures
 import com.mygdx.spacechoppers.model.ChopperModel
 import com.mygdx.spacechoppers.model.Joystick
 import com.mygdx.spacechoppers.view.AsteroidView
 import com.mygdx.spacechoppers.view.ChopperView
-
 
 
 class PlayState(gsm: GameStateManager) : GameState(gsm) {
@@ -77,5 +74,11 @@ class PlayState(gsm: GameStateManager) : GameState(gsm) {
 
         stage.act(Gdx.graphics.deltaTime)
         stage.draw()
+
+        // Render Box2D world using the debug renderer
+        val debugMatrix = Matrix4(cam.combined)
+        debugMatrix.scale(1 / PIXELS_TO_METERS, 1 / PIXELS_TO_METERS, 1f)
+        box2DDebugRenderer.render(world, debugMatrix)
+
     }
 }
