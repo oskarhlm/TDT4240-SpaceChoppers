b84c8e538a9c23e3a89f455f5f2c004f5cea30fc
Vetle Grim Hjelmtvedt <vetle.grim.hjelmtvedt@dnb.no>
2023-04-18 11:45:11 +0200

    start adding laser model

---
+++ SpaceChoppers/core/src/com/mygdx/spacechoppers/model/Laser.java
@@ -0,0 +1,12 @@
+package com.mygdx.spacechoppers.model;
+
+import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.math.Vector3;
+
+public class Laser {
+
+    private Vector3 position;
+    private Vector2 velocity;
+
+    
+}
