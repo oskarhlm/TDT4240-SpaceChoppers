a79e3935be21f7bc7b0ce6ea1da026f7a066f13d
Arild Grimstveit <arild@grimstveit.no>
2023-04-21 13:43:37 +0200

    add images to tutorial

--- SpaceChoppers/core/src/com/mygdx/spacechoppers/gamestates/menu/TutorialState.kt
+++ SpaceChoppers/core/src/com/mygdx/spacechoppers/gamestates/menu/TutorialState.kt
@@ -1,10 +1,14 @@
 package com.mygdx.spacechoppers.gamestates.menu
 
+import com.badlogic.gdx.graphics.g2d.Sprite
+import com.badlogic.gdx.graphics.g2d.TextureRegion
 import com.badlogic.gdx.scenes.scene2d.InputEvent
+import com.badlogic.gdx.scenes.scene2d.ui.Image
 import com.badlogic.gdx.scenes.scene2d.ui.ScrollPane
 import com.badlogic.gdx.scenes.scene2d.ui.Table
 import com.badlogic.gdx.scenes.scene2d.ui.TextButton
 import com.badlogic.gdx.scenes.scene2d.utils.ClickListener
+import com.mygdx.spacechoppers.AssetManager
 import com.mygdx.spacechoppers.GameStateManager
 import com.mygdx.spacechoppers.utils.MenuCommon
 
@@ -18,6 +22,32 @@ class TutorialState(gsm: GameStateManager) : MenuBase(gsm){
         val table = Table()
         val scroll = ScrollPane(table, skin)
 
+        val heliSprite = Sprite(AssetManager.heliTextures[0])
+        heliSprite.setScale(0.01f)
+        val heliImage = Image((heliSprite))
+
+        val asteroids = arrayListOf<Image>()
+        val asteroidTextures = AssetManager.asteroidSheet
+
+        val singleTextureWidth: Int = asteroidTextures.width / 4
+        val singleTextureHeight: Int = asteroidTextures.height / 4
+
+        for (row in 0 until 4) {
+            for (column in 0 until 4) {
+                val texture = TextureRegion(
+                    asteroidTextures,
+                    column * singleTextureWidth,
+                    row * singleTextureHeight,
+                    singleTextureWidth,
+                    singleTextureHeight
+                )
+                val asteroidSprite = Sprite(texture)
+                asteroidSprite.setScale(0.2f)
+                val asteroidImage = Image(asteroidSprite)
+                asteroids.add(asteroidImage)
+            }
+        }
+
         table.top()
         table.row()
         table.add(MenuCommon.scaledLabel("How To Play", 6f)).padBottom(100f)
@@ -28,9 +58,13 @@ class TutorialState(gsm: GameStateManager) : MenuBase(gsm){
         table.row()
         table.add(MenuCommon.scaledLabel("The chopper is the playable entity.", 2f)).left()
         table.row()
-        table.add(MenuCommon.scaledLabel("You control the chopper.", 2f)).left().padBottom(25f)
+        table.add(heliImage).center()
         table.row()
-        table.add(MenuCommon.scaledLabel("Asteroids", 3f)).left().padBottom(25f)
+        table.add(MenuCommon.scaledLabel("You control the chopper in space.", 2f)).left().padBottom(25f)
+        table.row()
+        table.add(MenuCommon.scaledLabel("Asteroids", 3f)).left()
+        table.row()
+        table.add(asteroids[0])
         table.row()
         table.add(MenuCommon.scaledLabel("The asteroids are the enemies", 2f)).left()
         table.row()
