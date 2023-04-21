95ae49d6008437c051623379796e7975dc6fd46e
Lars Holter <larsholter@gmail.com>
2023-04-21 12:38:48 +0200

    Now chooses the best server based on RTT

--- SpaceChoppers/core/src/com/mygdx/spacechoppers/networking/NetworkClient.java
+++ SpaceChoppers/core/src/com/mygdx/spacechoppers/networking/NetworkClient.java
@@ -3,8 +3,14 @@ package com.mygdx.spacechoppers.networking;
 import com.mygdx.spacechoppers.data.networking.Message;
 import com.mygdx.spacechoppers.data.networking.MessageAction;
 
+import java.io.IOException;
+import java.net.InetAddress;
 import java.net.URI;
 import java.net.URISyntaxException;
+import java.net.UnknownHostException;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
 import java.util.concurrent.CountDownLatch;
 
 
@@ -13,14 +19,34 @@ public class NetworkClient {
     private static NetworkClient instance = null;
     private final NetworkHandler handler;
     private final CountDownLatch latch;
-    private URI websocketURI;
 
     private NetworkClient() throws URISyntaxException {
         latch = new CountDownLatch(1);
-        websocketURI = new URI("ws://sc.hjelmtvedt.io:6968");
-        handler = new NetworkHandler(websocketURI, latch);
+        URI websocketURI1 = new URI("ws://sc.hjelmtvedt.io:6968");
+        URI websocketURI2 = new URI("ws://sc.holter.tech:6968");
+
+        List<URI> uris = Arrays.asList(websocketURI1, websocketURI2);
+        URI bestURI = null;
+        long bestPingRTT = Long.MAX_VALUE;
+
+        for (URI uri : uris) {
+            long currentPingRTT = getPingRTT(uri);
+            if (currentPingRTT < bestPingRTT) {
+                bestPingRTT = currentPingRTT;
+                bestURI = uri;
+            }
+        }
+
+        if (bestURI == null) {
+            throw new RuntimeException("No available URIs");
+        }
+
+        handler = new NetworkHandler(bestURI, latch);
+        System.out.println("Connected to: " + bestURI);
         handler.connect();
     }
+
+
 
     public static NetworkClient getInstance() throws URISyntaxException {
         if (instance == null) {
@@ -28,6 +54,21 @@ public class NetworkClient {
         }
         return instance;
     }
+
+    private long getPingRTT(URI uri) {
+        try {
+            InetAddress inetAddress = InetAddress.getByName(uri.getHost());
+            long startTime = System.currentTimeMillis();
+            if (inetAddress.isReachable(1000)) {
+                long endTime = System.currentTimeMillis();
+                return endTime - startTime;
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return Long.MAX_VALUE;
+    }
+
 
     public void createLobby(String username) throws InterruptedException {
         String createLobby = new Message.Builder()
